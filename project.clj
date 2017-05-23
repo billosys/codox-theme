29aa51b6d11b5fae59cfcfb84626522f0e50a98b
Chen Li <li@billo.systems>
2017-05-22 23:58:13 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojang/codox-theme "0.2.0-SNAPSHOT"
-  :description "A Codox Theme for Clojang Projects Documentation"
-  :url "https://github.com/clojang/codox-theme"
+(defproject systems.billo/superhero-codox-theme "0.3.0-SNAPSHOT"
+  :description "A Codox Theme for Bootswatch Superhero-themed Documentation"
+  :url "https://github.com/billosys/superhero-codox-theme"
   :license {:name "Eclipse Public License 1.0"
             :url "https://www.eclipse.org/legal/epl-v10.html"
-            :year 2016
+            :year 2017
             :key "epl-1.0"})
